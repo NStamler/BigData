b58664c0d9a006917074dd8f29ca495e74178b60
NStamler <NStamler@users.noreply.github.com>
2020-04-05 17:01:48 -0400

    Create config.js

---
+++ storytelling-master/src/vanilla-js/config.js
@@ -0,0 +1,51 @@
+var config = {
+    style: 'mapbox://styles/mapbox/dark-v10',
+    accessToken: 'pk.eyJ1Ijoic3RhbWxlcm4iLCJhIjoiY2s4bmh1NXR2MHltdzNnbzJnb3h3Ym5jeSJ9._-5EsjTgtexuxzt1nzw6QQ',
+    showMarkers: true,
+    theme: 'dark',
+    alignment: 'left',
+    title: 'Affordable and Luxury Housing in New York City',
+    subtitle: '11.154 Big Data 2020',
+    byline: 'By Natasha Stamler, Cesia Massott Alban, Val Shum',
+    footer: 'Source: source citations, etc.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            title: 'Display Title',
+            image: './path/to/image/source.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+                center: [-122.418398, 37.759483],
+                zoom: 13.5,
+                pitch: 60,
+                bearing: 0
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'other-identifier',
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-77.020636, 38.886900],
+                zoom: 13.5,
+                pitch: 60,
+                bearing: -43.2
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
